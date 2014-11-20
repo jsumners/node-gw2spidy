60569b6ebe468e4e07cb6b9df6d97e6ad3418c71
James Sumners <james.sumners@gmail.com>
2014-11-20 11:13:38 -0500

    Configures grunt-github-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,13 +7,22 @@ require('load-grunt-tasks')(grunt);
 grunt.initConfig({
   shell: {
     test: {
-      command: "node test/index.js"
+      command: 'node test/index.js'
     },
     doc: {
-      command: "./node_modules/.bin/jsdoc -c jsdoc.conf ."
+      command: './node_modules/.bin/jsdoc -c jsdoc.conf .'
+    }
+  },
+  githubPages: {
+    target: {
+      options: {
+        commitMessage: 'Publishing new documentation'
+      },
+      src: 'doc'
     }
   }
 });
 
 grunt.registerTask('test', ['shell:test']);
 grunt.registerTask('doc', ['shell:doc']);
+grunt.registerTask('publishdoc', ['githubPages:target']);
